be8a798b9fd208a4bea5fdc695d9cbb79f210864
trietlu <trietlu@amazon.com>
2021-02-28 11:55:38 -0500

    Re-enable Change Role functionality.  Removed ability to map group membership from IdP. Create Alert message if signed in  user is not Editor or Admin

--- frontend/src/containers/UserListing.tsx
+++ frontend/src/containers/UserListing.tsx
@@ -177,6 +177,15 @@ function UserListing() {
             <Button
               variant="outline"
               disabled={selected.length === 0}
+              onClick={changeRole}
+            >
+              Change role
+            </Button>
+          </span>
+          <span>
+            <Button
+              variant="outline"
+              disabled={selected.length === 0}
               onClick={onRemoveUsers}
             >
               Remove user(s)
